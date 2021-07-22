a5fe385a33fdc2b5d094efb3e35f21b0936c4881
lennijusten <ljusten@wisc.edu>
2021-07-22 17:36:03 +0200

    Add fix where corpus word mapping predefined for vectorization of subsets

--- corpus_similarity.py
+++ corpus_similarity.py
@@ -8,7 +8,6 @@
 # Chapman & Hall: New York. 2000. Section 14.7
 # Kilgarriff, Adam. (2001). Comparing Corpora. International Journal of Corpus Linguistics. 6. 10.1075/ijcl.6.1.05kil.
 
-from spearman_rho import Similarity
 import pandas
 import numpy as np
 import pickle
@@ -16,9 +15,13 @@ from sklearn.feature_extraction.text import CountVectorizer
 from scipy.stats import spearmanr
 
 # Load data with already text (pre-processed text recommended)
+data_path = 'Data/comment.pkl'  # comments over the entire corpus
 control_path = 'Data/df_control_s1-lazy.pkl'
 strat_path = 'Data/df_strat-lazy.pkl'
 eval_path = 'Data/df_eval_s1-lazy.pkl'
+
+with open(data_path, 'rb') as handle:
+    df = pickle.load(handle)
 
 with open(eval_path, 'rb') as handle:
     df_eval_s1 = pickle.load(handle)
@@ -29,12 +32,16 @@ with open(control_path, 'rb') as handle:
 with open(strat_path, 'rb') as handle:
     df_strat = pickle.load(handle)
 
+# Get indexed vocabulary for the entire corpus (words occuring at least three times)
+vectorizer1 = CountVectorizer(ngram_range=(1, 1), min_df=3)
+corpus = vectorizer1.fit_transform(df['text2'])
+corpus_vocab = vectorizer1.get_feature_names()
 
-vectorizer = CountVectorizer(ngram_range=(1, 1), min_df=3)
-
+# Vectorizer for subsets but with complete mapping for corpus vocab
+vectorizer2 = CountVectorizer(ngram_range=(1,1),min_df=3,vocabulary=corpus_vocab)
 
 def get_features(comments):
-    X = vectorizer.fit_transform(comments)
+    X = vectorizer2.fit_transform(comments)
     freq_array = X.toarray()
     freq_array_sum = np.sum(freq_array, axis=0)
 
@@ -45,10 +52,11 @@ def calculate_spear(corpus1, corpus2):
     features1 = get_features(corpus1)
     features2 = get_features(corpus2)
 
-    spear_results = spearmanr(features1, features2)[0]
+    spear_results = spearmanr(features1, features2)
 
     return spear_results
 
+# Returns spearman rho and associated p-value
 rho_eval_control = calculate_spear(df_eval_s1['text2'].values, df_control_s1['text2'].values)
 rho_eval_strat = calculate_spear(df_eval_s1['text2'].values, df_strat['text2'].values)
 rho_strat_control = calculate_spear(df_control_s1['text2'].values, df_eval_s1['text2'].values)
